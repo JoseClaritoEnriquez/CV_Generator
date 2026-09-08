050901f45d0f3df362cd194efb70b52997322f3f
Jose Clarito Enriquez <20140005962@my.xu.edu.ph>
2026-09-08 15:55:55 -0500

    created BasicInfo Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './styles/App.css'
+import BasicInfo from './components/BasicInfo';
 
 function App() {
   return (
-    <div className='app'>
+    <main className='app-container'>
       <header>
         <h1>CV Generator</h1>
       </header>
 
-
-    </div>
+      <BasicInfo />
+    </main>
   )
 }
 
---
+++ src/components/BasicInfo.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function BasicInfo() {
+    const [basicInfo, setBasicInfo] = useState({
+        name: '',
+        email: '',
+        phone: '',
+    });
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submitted data:', basicInfo);
+    }
+
+
+    return (
+        <div className="form-section">
+            <h2> Basic Information </h2>
+            <form onSubmit={handleSubmit}>
+                <label>Name:
+                    <input type="text" value={basicInfo.name}
+                        onChange={(e) => setBasicInfo({ ...basicInfo, name: e.target.value })} />
+                </label>
+
+                <label>Email:
+                    <input type="email" value={basicInfo.email}
+                        onChange={(e) => setBasicInfo({ ...basicInfo, email: e.target.value })} />
+                </label>
+                <label>phone:
+                    <input type="tel" value={basicInfo.phone}
+                        onChange={(e) => setBasicInfo({ ...basicInfo, phone: e.target.value })} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div >
+    );
+}
